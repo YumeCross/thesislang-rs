004b7ff8cac8f76f7fbf48473c122f7c9a1aee21
Flamme Shadow <FlammeShadow@outlook.com>
2024-07-05 01:44:12 +0800

    src/syntax.rs: Added enum `Node`.

--- src/syntax.rs
+++ src/syntax.rs
@@ -17,8 +17,37 @@ impl Into<String> for Symbol {
     fn into(self) -> String { self.0 }
 }
 
+#[derive(Debug, Clone)]
+pub enum Node {
+    List(Vec<Node>),
+    Symbol(Symbol)
+}
+
+impl Display for Node {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Node::List(nodes) => {
+                if nodes.is_empty() { return write!(f, "()"); }
+
+                write!(f, "({} ", nodes[0])?;
+                for node in nodes[1..nodes.len() - 1].iter() {
+                    write!(f, "{} ", node)?
+                }
+                write!(f, "{})", nodes.last().unwrap())
+            }
+            Node::Symbol(symbol) => write!(f, "{}", symbol)
+        }
+    }
+}
+
 mod tests {
-    use super::Symbol;
+    use super::{Node, Symbol};
+
+    #[test]
+    fn node_to_string() {
+        use Node::*;
+        assert_eq!(List(vec![Symbol("apply".into()), Symbol("+".into())]).to_string(), "(apply +)");
+    }
 
     #[test]
     fn symbol_to_string() {
